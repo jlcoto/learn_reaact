cb1f50b9257a8951b9376ebcc59d144bbabffe4f
jlcoto <jscoto@gmail.com>
2017-07-08 15:29:48 +0200

    Modifies Entry. Now it is a functional component.

--- src/Entry.js
+++ src/Entry.js
@@ -1,24 +1,14 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-
-
-class Entry extends Component {
-
-	static PropTypes = {
-		entries: PropTypes.array.isRequired,
-		onRemove: PropTypes.array.isRequired
-	}
+import React from 'react'
 
 
 
-	render() {
+function Entry(props) {
 		return (
 				<li>
-					{this.props.entry}
-					<button className="remove" onClick={() => this.props.onClick()}> x </button>
+					{props.entry}
+					<button className="remove" onClick={() => props.onClick()}> x </button>
 				</li>
 		)
-	}
 
 }
 
